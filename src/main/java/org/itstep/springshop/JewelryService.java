295994d270175f74755296f70b847235e9449926
zhenyka9696 <zhenyka9696@mail.ru>
2022-08-09 21:21:35 +0300

    Initial commit

---
+++ src/main/java/org/itstep/springshop/JewelryService.java
@@ -0,0 +1,2 @@
+package org.itstep.springshop;public class JewelryService {
+}
---
+++ src/main/java/org/itstep/springshop/Material.java
@@ -0,0 +1,24 @@
+package org.itstep.springshop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "jewelry")
+public class Jewelry {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String color;
+    private Double price;
+    private Double priceNew;
+    private byte[] image;
+}
